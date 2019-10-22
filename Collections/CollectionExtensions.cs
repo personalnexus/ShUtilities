92f88a670ca7ddb4de65a14c4b1db2f1177044e2
StefanH <personalnexus@users.noreply.github.com>
2019-10-22 21:24:00 +0200

    Added TryGetRange to return all values from SortedList whose keys fall into the given range.

--- Collections/CollectionExtensions.cs
+++ Collections/CollectionExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace ShUtilities.Collections
 {
@@ -119,6 +120,67 @@ namespace ShUtilities.Collections
             {
                 yield return partition;
             }
+        }
+
+        private static bool TryGetIndexOf<TKey>(IList<TKey> keys, IComparer<TKey> comparer, TKey key, out int index)
+        {
+            bool result = false;
+            index = 0;
+            int lowerBound = 0;
+            int upperBound = keys.Count - 1;
+
+            while (lowerBound <= upperBound && !result)
+            {
+                int pivot = lowerBound + (upperBound - lowerBound) / 2;
+                int comparisonResult = comparer.Compare(key, keys[pivot]);
+                if (comparisonResult == 0)
+                {
+                    result = true;
+                    index = pivot;
+                }
+                else if (comparisonResult < 0)
+                {
+                    upperBound = pivot - 1;
+                }
+                else
+                {
+                    lowerBound = pivot + 1;
+                }
+            }
+            if (!result)
+            {
+                index = lowerBound;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Tries to return all values from a <see cref="SortedList{TKey, TValue}"/> whose keys fall into the given range.
+        /// </summary>
+        /// <returns>True, if at least one matching value was found, otherwise false.</returns>
+        public static bool TryGetRange<TKey, TValue>(this SortedList<TKey, TValue> list, TKey lowerBound, TKey upperBound, out IEnumerable<TValue> values)
+        {
+            if (list.Comparer.Compare(lowerBound, upperBound) > 0)
+            {
+                throw new ArgumentException($"{nameof(lowerBound)} must be less than or equal to {nameof(upperBound)}.");
+            }
+
+            TryGetIndexOf(list.Keys, list.Comparer, lowerBound, out int lowerIndex);
+            int upperIndex = lowerIndex;
+            while (upperIndex < list.Count)
+            {
+                if (list.Comparer.Compare(upperBound, list.Keys[upperIndex]) < 0)
+                {
+                    break;
+                }
+                upperIndex++;
+            }
+            int valueCount = upperIndex - lowerIndex;
+
+            bool result = valueCount != 0;
+            values = result ? list.Values.Skip(lowerIndex).Take(valueCount) : Enumerable.Empty<TValue>();
+
+            return result;
         }
     }
 }
--- ShUtilitiesTest/CollectionExtensionsTest.cs
+++ ShUtilitiesTest/CollectionExtensionsTest.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ShUtilities.Collections;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using static ShUtilitiesTest.ExceptionUtility;
 
 namespace ShUtilitiesTest
 {
@@ -44,8 +46,6 @@ namespace ShUtilitiesTest
             Assert.AreEqual(5, partitions[2][0]);
         }
 
-
-
         [TestMethod]
         public void ToOnePartition()
         {
@@ -54,6 +54,38 @@ namespace ShUtilitiesTest
             Assert.AreEqual(1, partitions.Count);
             Assert.AreNotEqual(int.MaxValue, partitions[0].Capacity);
             CollectionAssert.AreEqual(items, partitions[0]);
+        }
+
+        [TestMethod]
+        public void TryGetRange()
+        {
+            var list = new SortedList<int, int>(Enumerable.Range(0, 1000).ToDictionary(x => x, x => x));
+            list.Remove(3);
+
+            Check(2, 2, true, 2);
+            Check(2, 3, true, 2);
+            Check(3, 4, true, 4);
+            Check(2, 4, true, 2, 4);
+            Check(2, 6, true, 2, 4, 5, 6);
+            Check(-1, 2, true, 0, 1, 2);
+            Check(999, 1002, true, 999);
+            Check(-1, 1002, true, list.Values.ToArray());
+
+            Check(3, 3, false);
+            Check(9999, 999999, false);
+
+            Expect<ArgumentException>(() => list.TryGetRange(3, -1, out _));
+
+            void Check(int lowerBound, int upperBound, bool expectedResult, params int[] expectedValues)
+            {
+                bool actualResult = list.TryGetRange(lowerBound, upperBound, out IEnumerable<int> values);
+                Assert.AreEqual(expectedResult, actualResult);
+                if (expectedResult)
+                {
+                    int[] actualValues = values.ToArray();
+                    CollectionAssert.AreEqual(expectedValues, actualValues, $"{lowerBound}-{upperBound}: {actualValues.ToDelimitedString(",")}");
+                }
+            }
         }
 
         private string CreateValue(string key)
