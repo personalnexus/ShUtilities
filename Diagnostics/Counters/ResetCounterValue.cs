5021dede34553572d4d5b20fdcd31c9cc481a97a
StefanH <personalnexus@users.noreply.github.com>
2021-11-05 21:48:32 +0100

    disable warning CS0169 for padding fields

--- Diagnostics/Counters/ResetCounterValue.cs
+++ Diagnostics/Counters/ResetCounterValue.cs
@@ -8,9 +8,15 @@ namespace ShUtilities.Diagnostics.Counters
         // Writing from one thread with padding to avoid false sharing https://mechanical-sympathy.blogspot.com/2011/07/false-sharing.html
         //
 
+        #pragma warning disable CS0169
         private long _padding1, _padding2, _padding3, _padding4, _padding5, _padding6, _padding7;
+        #pragma warning restore CS0169
+
         private long _written;
+        
+        #pragma warning disable CS0169
         private long _padding8, _padding9, _padding10, _padding11, _padding12, _padding13, _padding14;
+        #pragma warning restore CS0169
 
         public void Increment() => _written++;
 
