9eefb982f7a516c4825bad53d7a8793006c33b02
StefanH <personalnexus@users.noreply.github.com>
2021-11-07 22:00:12 +0100

    Moved padded value into its own struct type

--- Diagnostics/Counters/ResetCounterValue.cs
+++ Diagnostics/Counters/ResetCounterValue.cs
@@ -1,24 +1,17 @@
 ﻿using System.Threading;
+using ShUtilities.Threading;
 
 namespace ShUtilities.Diagnostics.Counters
 {
     internal class ResetCounterValue: IIncrementableCounter
     {
         //
-        // Writing from one thread with padding to avoid false sharing https://mechanical-sympathy.blogspot.com/2011/07/false-sharing.html
+        // Writing from one thread
         //
-
-        #pragma warning disable CS0169
-        private long _padding1, _padding2, _padding3, _padding4, _padding5, _padding6, _padding7;
-        #pragma warning restore CS0169
-
-        private long _written;
         
-        #pragma warning disable CS0169
-        private long _padding8, _padding9, _padding10, _padding11, _padding12, _padding13, _padding14;
-        #pragma warning restore CS0169
+        private Padded<long> _written;
 
-        public void Increment() => _written++;
+        public void Increment() => _written.Value++;
 
         //
         // Reading from one other thread
@@ -31,7 +24,7 @@ namespace ShUtilities.Diagnostics.Counters
         public void Update(bool isFirstOfTheDay)
         {
             long previousTotal = Total;
-            long newTotal = Volatile.Read(ref _written);
+            long newTotal = Volatile.Read(ref _written.Value);
             Current = newTotal - previousTotal;
             Total = newTotal;
             Today = isFirstOfTheDay ? Current : Today + Current;
