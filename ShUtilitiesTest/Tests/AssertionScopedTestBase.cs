f89b08453fd08d8874846ddd637e21e2a49d8040
StefanH <personalnexus@users.noreply.github.com>
2022-08-06 12:09:20 +0200

    Experimenting with FluentAssertions and DataTestMethod to improve unit tests

---
+++ ShUtilitiesTest/Tests/AssertionScopedTestBase.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions.Execution;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ShUtilitiesTest.Tests
+{
+    public class AssertionScopedTestBase
+    {
+        private AssertionScope _scope;
+
+        [TestInitialize]
+        public void Initialize() => _scope = new AssertionScope();
+
+        [TestCleanup]
+        public void Cleanup() => _scope.Dispose();
+    }
+}
--- ShUtilitiesTest/Tests/StringExtensionsTest.cs
+++ ShUtilitiesTest/Tests/StringExtensionsTest.cs
@@ -1,11 +1,16 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using FluentAssertions;
+using FluentAssertions.Execution;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ShUtilities.Text;
+using System;
 
 namespace ShUtilitiesTest.Tests
 {
     [TestClass]
     public class StringExtensionsTest
     {
+        // 1: Old style
+
         [TestMethod]
         public void IsNullOrEmpty()
         {
@@ -14,29 +19,53 @@ namespace ShUtilitiesTest.Tests
             Assert.IsTrue(((string)null).IsNullOrEmpty());
         }
 
-        [TestMethod]
-        public void In()
-        {
-            Assert.IsTrue("1".In("1", "2"));
-            Assert.IsTrue("1".In("1"));
-            Assert.IsFalse("1".In());
-            Assert.IsFalse("1".In(""));
-            Assert.IsFalse("1".In("2"));
-        }
+        // 2: DataTestMethod
+
+        [DataTestMethod]
+        [DataRow(new[] { "1", "2" }, DisplayName = "Multiple elements, one matching")]
+        [DataRow(new[] { "1", "1" }, DisplayName = "Multiple elements, multiple matching")]
+        [DataRow(new[] { "1" }, DisplayName = "Single matching element")]
+        public void In_IsTrue(string[] elements) => "1".In(elements).Should().BeTrue();
+
+        [DataTestMethod]
+        [DataRow(new string[0], DisplayName = "No elements")]
+        [DataRow(new[] { "2", "3" }, DisplayName = "Multiple elements, none matching")]
+        [DataRow(new[] { "2" }, DisplayName = "Single non-matching element")]
+        public void In_IsFalse(string[] elements) => "1".In(elements).Should().BeFalse();
+
+        // 3: FluentAssertions with AssertionScope for multiple assertions
 
         [TestMethod]
         public void Left()
         {
-            Assert.AreEqual("123", "123456".Left(3));
-            Assert.AreEqual("", "123456".Left(0));
+            using (new AssertionScope())
+            {
+                "123456".Left(0).Should().BeEmpty();
+                "123456".Left(3).Should().Be("123");
+                "123456".Left(6).Should().Be("123456");
+                "123456".Invoking(x => x.Left(9)).Should().Throw<ArgumentOutOfRangeException>();
+                "123456".Invoking(x => x.Left(-3)).Should().Throw<ArgumentOutOfRangeException>();
+            }
         }
 
+        // 4: FluentAssertions with one method per assertion
+
         [TestMethod]
-        public void Right()
-        {
-            Assert.AreEqual("456", "123456".Right(3));
-            Assert.AreEqual("", "123456".Right(0));
-        }
+        public void Right_CountIsZero_Empty() => "123456".Right(0).Should().BeEmpty();
+
+        [TestMethod]
+        public void Right_CountIsLessThanLength_Substring() => "123456".Right(3).Should().Be("456");
+
+        [TestMethod]
+        public void Right_CountEqualsLength_FullString() => "123456".Right(6).Should().Be("123456");
+
+        [TestMethod]
+        public void Right_CountIsMoreThanLength_ArgumentOutOfRangeException() => "123456".Invoking(x => x.Right(9)).Should().Throw<ArgumentOutOfRangeException>();
+        
+        [TestMethod]
+        public void Right_CountIsLessThanZweo_ArgumentOutOfRangeException() => "123456".Invoking(x => x.Right(-3)).Should().Throw<ArgumentOutOfRangeException>();
+
+        // TODO
 
         [TestMethod]
         public void Between()
