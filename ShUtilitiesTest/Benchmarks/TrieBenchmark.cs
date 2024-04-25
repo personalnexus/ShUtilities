52e6ab1ad3413c49d9364aad369fbe210a6d8285
StefanH <personalnexus@users.noreply.github.com>
2024-04-25 19:53:24 +0200

    Using a larger, more realistic set of possible characters in TrieBenchmark Dictionary=1.00 Trie=0.47 UnsafeTrie=0.34

--- ShUtilitiesTest/Benchmarks/TrieBenchmark.cs
+++ ShUtilitiesTest/Benchmarks/TrieBenchmark.cs
@@ -14,8 +14,8 @@ namespace ShUtilitiesTest.Benchmarks
             _unsafeTrie = AddExpectedKeyValuePairs(new UnsafeTrie<int>(PossibleCharacters, ItemCount - 2, 1000));
         }
 
-        private const int ItemCount = 1_000;
-        private readonly HashSet<char> PossibleCharacters = new HashSet<char> { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+        private const int ItemCount = 0x11111;
+        private readonly HashSet<char> PossibleCharacters = new HashSet<char>("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789");
 
         private readonly Trie<int> _trie;
         private readonly UnsafeTrie<int> _unsafeTrie;
