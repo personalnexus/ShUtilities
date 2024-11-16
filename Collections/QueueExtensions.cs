6eb0566fdf9e92c341fe001e2b36253065ab69bf
StefanH <personalnexus@users.noreply.github.com>
2024-11-16 19:51:19 +0100

    remove code no longer needed i .NET 8

--- Collections/QueueExtensions.cs
+++ Collections/QueueExtensions.cs
@@ -4,17 +4,6 @@ namespace ShUtilities.Collections
 {
     public static class QueueExtensions
     {
-        // .NET 4
-        ///// <summary>
-        ///// Tries to dequeue an item if the queue is not empty, in which case false is returned.
-        ///// </summary>
-        //public static bool TryDequeue<T>(Queue<T> queue, out T item)
-        //{
-        //    bool result = queue.Count > 0;
-        //    item = result ? queue.Dequeue() : default;
-        //    return result;
-        //}
-
         /// <summary>
         /// Dequeues up to <paramref name="maxDequeueCount"/> items or the number of items in the queue, whichever is smaller, returning them as an <see cref="IEnumerable{T}"/>.
         /// </summary>
